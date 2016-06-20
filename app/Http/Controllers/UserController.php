1e35242f39377ba7e2695cad56d8cfa89cb6a941
Edu <etg75@hotmail.com>
2016-06-20 13:15:57 -0500

    Agregado el DataTables
    DataTables a Usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = \SS\myModel\User::paginate(15);
+        $users = \SS\myModel\User::paginate(10);
         return View('UserViews.usuarios', compact('users'));
 
     }
